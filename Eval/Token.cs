c940ae76dea7b2bc75a6e3df301eb5bfc5bd6437
KichiginaTatiana <79126796203@yandex.ru>
2017-04-09 12:37:55 +0700

    GetTokensFromString

--- Eval/Token.cs
+++ Eval/Token.cs
@@ -12,9 +12,47 @@ namespace EvalTask
         Number,
         Constant
     }
+
     public class Token
     {
         public TokenType Type { get; set; }
         public string Value { get; set; }
+
+        public Token()
+        {
+        }
+
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public const string operations = @"+-*/";
+
+        public static List<Token> GetTokensFromString(string input)
+        {
+            var result = new List<Token>();
+            var value = "";
+            foreach (var t in input)
+            {
+                if (!operations.Contains(t.ToString()))
+                    value += t;
+                else
+                {
+                    result.Add(char.IsDigit(value[0])
+                        ? new Token(TokenType.Number, value)
+                        : new Token(TokenType.Constant, value));
+                    result.Add(new Token() {Type = TokenType.Operation, Value = t.ToString()});
+                    value = "";
+                }
+            }
+            result.Add(char.IsDigit(value[0])
+                ? new Token(TokenType.Number, value)
+                : new Token(TokenType.Constant, value));
+
+            return result;
+        }
+
     }
 }
