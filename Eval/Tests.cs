141d079e34c82154b38b2f1fe635aa9b9ce29a72
Kostya Kulikov <pavko@protonmail.com>
2017-04-09 10:51:27 +0500

    Basic Eval functionality

---
+++ Eval/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EvalTask
+{
+    [TestFixture]
+    public class Calculator_Should
+    {
+        private Calculator calc;
+        [SetUp]
+        public void SetUp()
+        {
+            calc = new Calculator();
+        }
+
+        [Test]
+        public void SumsTwoIntValues()
+        {
+            List<string> input = new List<string>{"1", "+", "2"};
+
+            var res = calc.Calculate(input);
+
+            Assert.AreEqual(3,res);
+        }
+
+        [Test]
+        public void DividesTwoIntValues()
+        {
+            List<string> input = new List<string>{"4", "/", "2"};
+
+            var res = calc.Calculate(input);
+
+            Assert.AreEqual(2,res);
+        }
+
+        [Test]
+        public void MulipliesTwoIntValues()
+        {
+            List<string> input = new List<string> { "4", "*", "2" };
+            var res = calc.Calculate(input);
+
+            Assert.AreEqual(8, res);
+        }
+
+        [Test]
+        public void SubstractsTwoIntValues()
+        {
+            List<string> input = new List<string> { "4", "-", "2" };
+            var res = calc.Calculate(input);
+
+            Assert.AreEqual(2, res);
+        }
+    }
+}
